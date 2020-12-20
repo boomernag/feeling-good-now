13eac6c1b32b647590df7c372a181216da5ba84b
boomernag <william.dahlberg.n@gmail.com>
2020-12-20 12:40:56 +0000

    Added toasts

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 # Create your views here.
 
 
@@ -11,14 +14,28 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if size:
+        if item_id in list(cart.keys()):
+            if size in cart[item_id]['items_by_size'].keys():
+                cart[item_id]['items_by_size'][size] += quantity
+            else:
+                cart[item_id]['items_by_size'][size] = quantity
+        else:
+            cart[item_id] = {'items_by_size': {size: quantity}}
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
